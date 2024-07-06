bb341c34d37cb4e944724c1bc3fdb7281606ef24
asnganje <abd.nganje@gmail.com>
2024-07-06 12:24:40 +0300

    Add signup and login routes

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { createUser, getAllUsers } = require('../controllers/authController');
+const { createUser, getAllUsers, loginUser } = require('../controllers/authController');
 const router = express.Router();
 
-router.route('/').post(createUser).get(getAllUsers)
-
+router.route('/').get(getAllUsers)
+router.route('/signup').post(createUser)
+router.route('/login').post(loginUser)
 module.exports = router
