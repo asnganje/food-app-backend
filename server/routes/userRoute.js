6609d9dac7600daae425493886868c1538fd08f0
asnganje <abd.nganje@gmail.com>
2024-07-06 16:11:03 +0300

    Add logout logic to user routes

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createUser, getAllUsers, loginUser } = require('../controllers/authController');
+const { createUser, getAllUsers, loginUser, logoutUser } = require('../controllers/authController');
 const router = express.Router();
 
 router.route('/').get(getAllUsers)
 router.route('/signup').post(createUser)
 router.route('/login').post(loginUser)
+router.post('/logout', logoutUser)
 module.exports = router
